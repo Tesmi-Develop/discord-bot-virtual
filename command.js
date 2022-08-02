644c7b693befe0efd54f947609f027523435947e
Tornado-Technology <tornado.technologys@gmail.com>
2022-08-02 14:10:50 +1000

    New author command

--- command.js
+++ command.js
@@ -103,3 +103,7 @@ Command.add('идея', 'Напишет идею в канал для идей. 
         message.delete();
     });
 });
+
+Command.add('авторы', 'Показывает авторов бота', (client, message) => {
+    message.reply('> **Авторы**\n> -**Tesmi#204:**\n>   *YouTube*: https://www.youtube.com/channel/UC6reI1tVxc0IytdlpjgIFNQ\n>   *GitHub*: https://github.com/Tesmi-Devlop\n> -**TornadoTech#3699:**\n>   *Cite*: https://tornado-technology.github.io/Tornado-Technology/');
+});
